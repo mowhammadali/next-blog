103238aca2619c8dd281a8f6bbc25896ef2ff189
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2025-06-02 17:23:43 +0330

    create not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,22 @@
+"use client";
 import React from 'react'
+import css from "@/styles/not-found.module.css";
+import Text from '@/ui/text/text';
+import Button from '@/ui/button/button';
+import { FaArrowRightLong } from "react-icons/fa6";
+import { useRouter } from 'next/navigation';
 
-const NotFound = () => {
+const NotFound = (): React.JSX.Element => {
+  const router = useRouter();
+
   return (
-    <div>NotFound</div>
+    <div className={css.container}>
+      <Text as='p'>صفحه ای که دنبالش بودید ، پیدا نشد.</Text>
+      <Button bg='transparent' textColor='var(--text-base-600)' fontSize='16px' onClick={() => router.back()}>
+          <FaArrowRightLong className={css["arrow-right-icon"]}/>
+          برگشت
+      </Button>
+    </div>
   )
 }
 
